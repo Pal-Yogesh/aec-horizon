4a5f56c032f16e40584b276a5d232db521a99181
Yogesh Pal <yogeshpal5049@gmail.com>
2025-08-28 23:34:04 +0530

    done

--- app/page.js
+++ app/page.js
@@ -1,9 +1,13 @@
+import Banner from "@/components/Banner";
 import Navbar from "@/components/Navbar";
+import WhyChoose from "@/components/WhyChoose";
 
 export default function Home() {
   return (
     <>
     <Navbar />
+    <Banner />
+    <WhyChoose />
     </>
     
   );
---
+++ components/Banner.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <>
+      <div className="pt-32">
+        <div>
+          <div className="flex justify-center items-center">
+            <div>
+              <h1 className="text-[#37116C] font-semibold text-[60px]">
+                Three Infinite Loops,
+              </h1>
+              <h2 class="gradient-text font-semibold text-[60px]">
+                One Global Impact.
+              </h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Banner;
