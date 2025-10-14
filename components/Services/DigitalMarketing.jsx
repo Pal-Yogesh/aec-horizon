abda5fc74c1ff5b8b91605e02ba63e31cd32cb1f
Yogesh Pal <yogeshpal5049@gmail.com>
2025-10-15 00:40:03 +0530

    done

--- components/Services/DigitalMarketing.jsx
+++ components/Services/DigitalMarketing.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import TD from './TechnicalDevelopment/TD'
-import TDProjects from './TechnicalDevelopment/TDProjects'
-import TDServiceLocation from './TechnicalDevelopment/TDServiceLocation'
+import DM from './DigitalMarketing/DM'
+import DMProjects from './DigitalMarketing/DMProjects'
+import DMServiceLocation from './DigitalMarketing/DMServiceLocation'
 
 const DigitalMarketing = () => {
   return (
-    <div className="bg-gradient-to-b from-[#BCC8F2] to-[#E1E8FF] w-full pb-32  ">
-      <TD />
-      <TDProjects />
-      <TDServiceLocation />
+    <div className="bg-gradient-to-b from-[#D5CBF0] to-[#E3DDFF] w-full pb-32  ">
+      <DM />
+      <DMProjects />
+      <DMServiceLocation />
     </div>
   )
 }
